5c6967e43af6221d82aeb1117740a6f6392b7c37
dcccc <924890924@qq.com>
2016-04-06 12:14:16 +0800

    done

--- task_detail.py
+++ task_detail.py
@@ -11,16 +11,16 @@ def task_detail():
 	print "-"*120
 	print  "%-10s%-10s%-60s%s"  %("software", "user", "task neme", "pid")
 	print "-"*120
-	task = os.popen("top -b -n 1 | grep -E 'pw.x|dmol3.exe|castep|vasp'").readlines()
+	task = os.popen("top -b -n 1 | grep -E 'pw.x|dmol3.exe|castep|vasp|neb.x'").readlines()
 
 	core_num=[]
-	for soft in ["pw.x","dmol3", "castep", "vasp"]:
+	for soft in ["pw.x","dmol3", "castep", "vasp","neb.x"]:
 		pw = sorted(filter(lambda x: soft in x,task))
 		core_num.append(len(pw))
 		for i in range(0,len(pw),4):
 			line = re.split(r'\s+',pw[i].strip())
-			pid_of=os.popen("lsof -p "+line[0]+" | egrep 'cwd|home'").readlines()
-			if soft == "pw.x" :
+			pid_of=os.popen("lsof -p "+line[0]+" | egrep 'cwd|tmp'").readlines()
+			if soft in ["pw.x","neb.x"] :
 				cwd = re.split(r'\s+',pid_of[0].strip())[-1]
 				task_name= re.split(r'\/',pid_of[-1].strip())[-1]
 				cwd = cwd + "/" + ".".join(re.split(r"\.",task_name)[:-1]) + ".in"
@@ -30,6 +30,7 @@ def task_detail():
 			print "%-10s%-10s%-60s%-10d%-10d%-10d%-10d" %(soft, line[1], cwd, pid, pid+1, pid+2, pid+3)
 
 	print "-"*120
-	print  "processes in running     %d + %d + %d + %d"  %(core_num[0], core_num[1], core_num[2], core_num[3])
+	print  "processes in running     %s"  %(" + ".join(map(str,core_num)))
 	print "-"*120
+	
 task_detail()
