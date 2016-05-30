a7a9c33383e1b4f87da73b9799a4dc05d67dfceb
dcccc <924890924@qq.com>
2016-05-30 15:36:18 +0800

    update

--- endnote paper citation.py
+++ endnote paper citation.py
@@ -1,23 +1,33 @@
 #coding=utf-8
-import math
-import random
-import sys
-import time
-import numpy as np
 import re
+import glob
 
-file=open("scholar.enw")
 
 
-article=[]
-for i, line in enumerate(file):
-	article.append(line[3:].strip())
+def to_md(enw):
+	article=[]
+	file=open(enw)
+	for i, line in enumerate(file):
+		article.append(line[3:].strip())
+	
+	#print article
+	Au_num=0
+	for i in xrange(len(article)):
+		if "," in article[i]:
+			Au=re.split(r'\,\s',article[i])
+			Au[1]=re.sub(r'[a-z]+','',Au[1])
+			article[i]= " ".join(Au)
+			Au_num+=1
 
-#print article
-for i in [2,3,4]:
-	Au=re.split(r'\,\s',article[i])
-	Au[1]=re.sub(r'[a-z]+','',Au[1])
-	article[i]= " ".join(Au)
+	
+	md_txt= "%s. %s[%s]. *%s*, %s, %s(%s): %s."    %(", ".join(article[2:Au_num+2]), article[1], article[0][0],    
+	                                     article[Au_num+2], article[Au_num+7], article[Au_num+3],  article[Au_num+4], article[Au_num+5])
+	return md_txt
 
-print "%s. %s[%s]. *%s*, %s, %s(%s): %s."    %(", ".join(article[2:5]), article[1], article[0][0],    
-                                            article[5], article[9], article[6],  article[7], article[8])
+txt=open("markdown.md","a")
+
+all_enw=glob.glob("*.enw")
+
+print all_enw
+for i in all_enw:
+	print to_md(i)
